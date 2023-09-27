ef8e27afe5f75338737d6eb2a9f86e8eedc3addc
Ryan Roussel <rroussel@slac.stanford.edu>
2023-09-27 11:03:32 -0500

    Update execute_notebooks.bash

--- scripts/execute_notebooks.bash
+++ scripts/execute_notebooks.bash
@@ -1,18 +1,31 @@
 #!/bin/bash
 
+# Define an array of SKIP values
+SKIP=("parallel" "mobo")
+
 NOTEBOOKS=$(find . -type f -name "*.ipynb" -not -path '*/.*')
 
-SKIP="parallel" 
+echo "$NOTEBOOKS"
 
-echo $NOTEBOOKS
+# Function to check if a file should be skipped
+should_skip() {
+    local file="$1"
+    for skip_value in "${SKIP[@]}"; do
+        if [[ "$file" == *"$skip_value"* ]]; then
+            return 0 # Skip
+        fi
+    done
+    return 1 # Don't skip
+}
 
 for file in $NOTEBOOKS
 do
-    if [[ "$file" == *"$SKIP"* ]]; then 
+    if should_skip "$file"; then
         echo "Skipping $file"
         continue
     fi
-  
+
     echo "Executing $file"
-    jupyter nbconvert --to notebook --execute $file --inplace
+    jupyter nbconvert --to notebook --execute "$file" --inplace
 done
+
